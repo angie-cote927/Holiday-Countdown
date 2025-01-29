e943733870d478eb3274884448d6f6d077efe38f
angie-cote927 <mcote7243@gmail.com>
2025-01-28 18:21:31 -0700

    0.3
    Started the correct JS like the addTimer() function and created a timer name const.

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
-function updateCountdown(){
-    const targetDate = new Date(document.getElementById('date')); 
-    const play = document.getElementById("timer")
-    play.innerHTML = '';
+function updateCountdown(timer){
+    const now = new Date();
+            const timeDifference = targetDate - now; // Difference IN MILLISECONDS between current and target date
+            if(timeDifference <= 0){
+                document.getElementBYId("time").innerText = "The time of love is here!"
+                clearInterval(intervalID); // Stop the timer when the target date is reached
+                return;
+            }
 
-    const now = new Date()
-    const timeDifference = document.getElementById("timer1") - now; // Difference IN MILLISECONDS between current and target date
-    if(timeDifference <= 0){
-        play.innerText = "The time has come!"
-        clearInterval(intervalID); // Stop the timer when the target date is reached
-        return;
-    }
-    else {
+            // Calculate days, hours, minutes, seconds
+            const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
+            const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-    // Calculate days, hours, minutes, seconds
-        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
-        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+            // Display the countdown
+            document.getElementById("timer").innerText = `Days: ${days}, Hours: ${hours}, Minutes: ${minutes}, Seconds: ${seconds}`
+}
 
-        play.innerHTML += `<p>Days: ${days}, Hours: ${hours}, Minutes: ${minutes}, Seconds: ${seconds}</p>`;
-    }
-    const intervalID = setInterval(updateCountdown, 1000); // Update every second
-    }
-
-    // Display the countdown
-
-
-
-//Start the timer
-
-// Call the function once you click submit
+function addTimer(){
+    const eventName = document.getElementById("eventName")
+    const targetDate = new Date(document.getElementById("date"))
+    if (!eventName) {alert("Please type in a name")}
+    
+    const newTimer = document.createElement("div")
+    newTimer.innerHTML = `<div><h2 id="timerName"></h2>`
+}
 
