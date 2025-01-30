5c7637afa46f19999ad6a18ba03d1fe0eab3deba
angie-cote927 <mcote7243@gmail.com>
2025-01-30 16:31:17 -0700

    1.0
    Finished the HTML, JS, and CSS for all of the countdowns. Made it so that you can make as many countdowns as you want.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+let timers = []
+
 function updateCountdown(timer){
     const now = new Date();
-            const timeDifference = targetDate - now; // Difference IN MILLISECONDS between current and target date
+            const timeDifference = timer.targetDate - now; // Difference IN MILLISECONDS between current and target date
             if(timeDifference <= 0){
-                document.getElementBYId("time").innerText = "The time of love is here!"
+                timer.element.innerText = "The time of love is here!"
                 clearInterval(intervalID); // Stop the timer when the target date is reached
                 return;
             }
@@ -14,15 +16,30 @@ function updateCountdown(timer){
             const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
             // Display the countdown
-            document.getElementById("timer").innerText = `Days: ${days}, Hours: ${hours}, Minutes: ${minutes}, Seconds: ${seconds}`
+            console.log(timer.element)
+            timer.element.querySelector("#timed").innerText = `Days: ${days}, Hours: ${hours}, Minutes: ${minutes}, Seconds: ${seconds}`
 }
 
 function addTimer(){
-    const eventName = document.getElementById("eventName")
-    const targetDate = new Date(document.getElementById("date"))
+    const timerContainer = document.getElementById("timer-container")
+    const eventName = document.getElementById("eventName").value
+    const targetDate = new Date(document.getElementById("date").value)
+    
     if (!eventName) {alert("Please type in a name")}
     
     const newTimer = document.createElement("div")
-    newTimer.innerHTML = `<div><h2 id="timerName"></h2>`
-}
+    newTimer.innerHTML = `
+        <h2 id="timerName">${eventName}</h2>
+        <div id="timed">.. </div>`
+    const timer = {
+        name: eventName,
+        targetDate: targetDate,
+        element: newTimer,
+        timer: setInterval(() => updateCountdown(timer), 1000)
+    }
 
+    timerContainer.appendChild(newTimer)
+    timers.push(timer)
+    console.log(timer.targetDate)
+    updateCountdown(timer)
+}
